303d1963f213be410d0d06075a02192ae667757e
chuey1a <67492086+chuey1a@users.noreply.github.com>
2021-12-17 00:42:26 +1100

    Create README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# psychic-barnacle
+
+Codecademy project!
+
+A connect four game which can be played in the terminal. All the usual rules apply and players input there turns one after the other.
--- connect4.py
+++ connect4.py
@@ -1,7 +1,28 @@
 #connect 4
 
+def print_board(board):
+  print("  0   1   2   3   4   5   6  ")
+  print("-----------------------------")
+  for i in range(6):
+    print("| ", end = "")
+    for j in range(7):
+      if board[j][i] == 0:
+        print(" " + " | ", end = "")
+      else:
+        print(str(board[j][i]) + " | ", end = "")
+
+    print()
+    print("-----------------------------")
+  print()
+
+def input_player1(col):
+  if col > 7:
+    return 0
+
+
 board = []
 
 rows, cols = (7, 6)
 board = [[0]*cols]*rows
-print(board)
+
+print_board(board)
